7294d690ca2cf5ec7ace41b97c00046e9e29ab4e
Divya Mishra <76445851+divyamishra1908@users.noreply.github.com>
2022-07-09 10:51:36 +0530

    Time: 188 ms (95.23%), Space: 78.6 MB (81.22%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        long cursor = 0;
+        deque<int> dq;
+        
+        int i = nums.size() - 1;
+        for(;i >= 0; i--)
+        {
+            cursor = nums[i] + (dq.empty() ? 0 : nums[dq.front()]);
+            //cout << cursor << endl;
+            while(!dq.empty() and cursor > nums[dq.back()]) // always keep the larger value at the back.
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);   
+            
+            if(dq.front() >= i+k) dq.pop_front();
+            
+            nums[i] = cursor;
+        }
+        return cursor;
+    }
+};
